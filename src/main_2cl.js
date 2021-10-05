3f3ee8ef2109affea68a0957f014db5335c87a5a
2cleanwater <32264455+2cleanwater@users.noreply.github.com>
2021-10-05 18:53:55 +0900

    Add files via upload

--- src/main_2cl.js
+++ src/main_2cl.js
@@ -1,16 +1,25 @@
 
 // Json에 있는 파일을 받아오는 것
 function loaditems() {
-    return fetch('../data/data_2cl.json')
+    return fetch('data/data_2cl.json')
     .then(response => response.json())
     .then(json => json.items);
 }
 
+function displaytitems(items) {
+    const container = document.querySelector('.items');
+    container.innerHTML = items.map(item => createHTMLString(item));
+}
+
+function createHTMLString(item) {
+    return
+}
 //main
 
 loaditems()
     .then(items => {
-        // displatitems(items);
+        console.log(items);
+        displaytitems(items);
         // setEventListeners(items)
     })
     .catch(console.log);
